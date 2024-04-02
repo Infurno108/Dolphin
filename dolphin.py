e701a0a2b2dda86f8a5a2170478646640889f3f7
Flint Rose <oliverflintrose@gmail.com>
2024-04-02 12:23:29 -0500

    Ip input corrected in send function

--- dolphin.py
+++ dolphin.py
@@ -10,13 +10,20 @@ choice = input("Enter 1 for server and 2 for client: ")
 
 if choice == '1':
     ip = input("Please enter your ip address: ")
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # tcp
-    # Currently rigidly my internal ipv4, the actual app will just use the cloud box as the listener
-    server.bind((ip, 9999))
-    server.listen()
+    # Socket() -> creates a new socket, AF_INET saying ipv4, and sock_stream saying tcp
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    client, _ = server.accept()  # accepted client
-    client.send(public_key.save_pkcs1("PEM"))  # sending public key to client
+    # Bind() -> binds the socket to adress and port number
+    server.bind((ip, 9999))
+
+    server.listen()  # Listen() -> listens for incoming connections
+
+    # accpet() recieves connections, and returns touple: (socket, address of new connection)
+    client, _ = server.accept()
+    print(_)
+
+    # sending public key to client first
+    client.send(public_key.save_pkcs1("PEM"))
     public_partner = rsa.PublicKey.load_pkcs1(
         client.recv(1024))  # recieving public key from client
 
@@ -24,7 +31,7 @@ elif choice == '2':
     ip = input("Please enter the server's ip address: ")
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # currently just looping internally on my own machine for testing, this will need to be changed
-    client.connect(("192.168.1.69", 9999))
+    client.connect((ip, 9999))
     public_partner = rsa.PublicKey.load_pkcs1(
         client.recv(1024))  # receive first, send second
     client.send(public_key.save_pkcs1("PEM"))
